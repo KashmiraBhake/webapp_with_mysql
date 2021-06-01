e3da4d8b2b7d629fa0c4e4e32cf07f1031bc66f6
KashmiraBhake <kashmirabhake@gmail.com>
2021-06-01 17:15:24 +0000

    basic patient code

---
+++ src/main/java/com/karkinos/webapp/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.karkinos.webapp;
+ 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findByFirstName(String firstName);
+ 
+}
