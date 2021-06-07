e8213a9d5c7ebab24ea29889fe7e20e6de3c0bea
bkashmira <kashmiragithub@gmail.com>
2021-06-07 10:05:32 +0000

    work in progress

--- src/main/java/com/karkinos/webapp/Documents.java
+++ src/main/java/com/karkinos/webapp/Documents.java
@@ -1,16 +1,15 @@
 package com.karkinos.webapp;
 
-import javax.persistence.CascadeType;
+//import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
+//import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-//import javax.persistence.Transient;
 import javax.persistence.Transient;
 
 //import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -23,8 +22,8 @@ public class Documents {
   private Long docId;
   private String docName;
 
-  @ManyToOne(optional = true, cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-  @JoinColumn(name = "patient_id",referencedColumnName = "id")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "patient_id",nullable = false)
   private Patient patient;
     
   protected Documents() {
